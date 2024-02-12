607c387e656fe75c7c4eed3a71d8086704dc30a4
JoseDmultimedia <garciadiosaj@gmail.com>
2024-02-11 23:11:02 -0500

    feat: navbar layout without styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from './pages/home/HomePage';
+import { SectionOrdenPage } from './pages/pasarela-orden/SectionOrdenPage';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/section-orden" element={<SectionOrdenPage />}></Route>
+      </Routes>
+    </Router>
+  </div>
   );
 }
 
---
+++ src/pages/pasarela-orden/SectionOrdenPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function SectionOrdenPage() {
+  return (
+    <div>sectionOrdenPage</div>
+  )
+}
